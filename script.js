67ca5c0e77203e31af3d066943fdfee0172f7f91
Scott Johnston <137368865+smithdotsco@users.noreply.github.com>
2023-11-21 12:48:16 +0900

    added input prompt to get user board size

--- script.js
+++ script.js
@@ -15,9 +15,32 @@ function createBoard (size){
         div.style.backgroundColor = "yellow";
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function getSize() {
+    let input = prompt("What size will your board be?");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if (input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 & 100"
+    }
+    else{
+        message.innerHTML = ("Number accepted")
+    }
+}
+
+
+
+
+
+
+
+
     // for (let i = 0; i < size * size; i++) {
     //     let cell = document.createElement("div");
     //     cell.classList.add("cell");
     //     board.appendChild(cell);
     // }
-}
+
