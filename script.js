a7298a10e9671df5413f443730e8725dcea82b89
Scott Johnston <137368865+smithdotsco@users.noreply.github.com>
2023-11-21 12:11:24 +0900

    changed grid system

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ function createBoard (size){
     
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;    
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;   
-
-    for (let i = 0; i < size * size; i++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        board.appendChild(cell);
+    
+    let numDivs = size *size;
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
     }
+    // for (let i = 0; i < size * size; i++) {
+    //     let cell = document.createElement("div");
+    //     cell.classList.add("cell");
+    //     board.appendChild(cell);
+    // }
 }
