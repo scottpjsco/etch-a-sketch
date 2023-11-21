77fd7e4b1adc69ed4f64e703a51e3ee7c295bf70
Scott Johnston <137368865+smithdotsco@users.noreply.github.com>
2023-11-21 15:41:21 +0900

    added random color to button and black to black button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function (){
     createBoard(16);
 
     let btn_popup = document.querySelector ("#popup");
     btn_popup.addEventListener("click", function() {
         let size = getSize();
+        
         createBoard(size);
 
     })
@@ -19,9 +22,7 @@ function createBoard (size){
     let numDivs = size *size;
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.addEventListener("mouseover", function(){
-            div.style.backgroundColor = "black";
-        })
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
 }
@@ -33,12 +34,26 @@ function getSize() {
         message.innerHTML = "Please provide a number";
     }
     else if (input < 0 || input > 100){
-        message.innerHTML = "Provide a number between 1 & 100"
+        message.innerHTML = "Provide a number between 1 & 100";
     }
     else{
-        message.innerHTML = ("Number accepted")
+        message.innerHTML = ("Number accepted");
         return input;
     }
+}
+
+function colorDiv (){
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random()* 360}, 100%, 50%)`;
+    }
+    else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor (colorChoice){
+     color = colorChoice;
+    
 }
 
 
