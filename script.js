13de9e722e805fa1ae5c7c8162f43efd20a78eef
Scott Johnston <137368865+smithdotsco@users.noreply.github.com>
2023-11-24 10:23:40 +0900

    click to draw added

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function (){
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click){
+                draw.innerHTML = "Can Now Draw";
+            }
+            else {
+                draw.innerHTML = "Cannot Draw";
+            }
+        }
+
+    })
 
     let btn_popup = document.querySelector ("#popup");
     btn_popup.addEventListener("click", function() {
@@ -43,12 +58,14 @@ function getSize() {
 }
 
 function colorDiv (){
-    if (color == "random") {
+    if(click){
+        if (color == "random") {
         this.style.backgroundColor = `hsl(${Math.random()* 360}, 100%, 50%)`;
     }
     else {
         this.style.backgroundColor = 'black';
     }
+}
 }
 
 function setColor (colorChoice){
